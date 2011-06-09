b39aa0fdbed46196da462d970d66e80df5f42eec
cngithub <174700@qq.com>
2011-06-09 10:57:43 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ForumDemo::Application.config.secret_token = '42218f39ed872e5dc866a18a8885a0e5554b6fc83950d2dba98caa38cc90548df37ce848952850e759d1d5865c6f2f6bcc2311f285306b8afa516a4b1b3dd433'
