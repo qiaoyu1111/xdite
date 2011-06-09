bccfb9523035eab2ef11178b5292f1d8bd97f935
cngithub <174700@qq.com>
2011-06-09 19:30:52 +0800

    posts crud

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,53 @@
 class PostsController < ApplicationController
   def index
+    @posts=Post.all
   end
 
   def show
-  end
-
-  def edit
+    @post=Post.find(params[:id])
   end
 
   def new
+    @post=Post.new
+  end
+  
+  def edit
+    @post=Post.find(params[:id])
   end
 
+  def create
+    @post=Post.new(params[:post])
+    respond_to do |format|
+      if @post.save
+        #format.html {redirect_to :action => "show", :id => @post.id,:notice => "成功创建" } 输出不了notice这个参数
+        format.html {redirect_to(@post,:notice => "成功创建" )}
+        else 
+        format.html { redirect_to :action => "new" }
+      end
+    end
+  end
+  
+  def update
+    @post=Post.find(params[:id])
+    respond_to do |format|
+      if @post.update_attributes(params[:post])
+          format.html { redirect_to(@post,:notice => "成功更新")}
+        else
+          format.html { redirect_to :action => "update"}             
+      end    
+
+    end
+
+  
+  end 
+  
+  def destroy
+    @post=Post.find(params[:id])
+    @post.destroy
+    respond_to do |format|
+      format.html { redirect_to(@post) }
+    end
+          
+  end
+    
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,4 @@
 class Post < ActiveRecord::Base
+  validates_presence_of :title, :on => :create, :message => "can't be blank"
+  validates_presence_of :content, :on => :create, :message => "can't be blank"
 end
