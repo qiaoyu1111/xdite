d5b4483d31e1cf51503c4048ecc295c07215e1aa
cngithub <174700@qq.com>
2011-06-11 22:24:46 +0800

    增加分页功能

--- RubyCocoa-1.0.0/framework/build/RubyCocoa.build/Default/RubyCocoa.build/Script-4D7AD1190BF2994500F1A2C6.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-if [ -d bridge-support ]; then
-  rm -rf $BUILT_PRODUCTS_DIR/RubyCocoa.framework/Resources/BridgeSupport
-  mkdir -p $BUILT_PRODUCTS_DIR/RubyCocoa.framework/Resources/BridgeSupport
-  cp bridge-support/*.bridgesupport bridge-support/*.dylib $BUILT_PRODUCTS_DIR/RubyCocoa.framework/Resources/BridgeSupport
-fi
