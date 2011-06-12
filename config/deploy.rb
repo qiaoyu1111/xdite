60c078fa232e9e6091bca1cf41746e996f937c07
cngithub <174700@qq.com>
2011-06-12 22:40:17 +0800

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,2 +1,5 @@
-require 'capistrano/ext/multistage' require 'bundler/capistrano' #Using bundler with Capistrano
-set :stages, %w(staging production) set :default_stage, "production"
+require 'capistrano/ext/multistage' 
+require 'bundler/capistrano' #Using bundler with Capistrano
+
+set :stages, %w(staging production) 
+set :default_stage, "production"
